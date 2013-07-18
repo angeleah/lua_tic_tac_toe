9624ac8601912e30825f0f07a8c92db31c194558
Angeleah Daidone <angeleah@gmail.com>
2013-07-17 22:37:25 -0500

    Add set, get, and undo move

--- spec/board_spec.lua
+++ spec/board_spec.lua
@@ -1,12 +1,30 @@
 require "busted"
 require "board"
+require "inspect"
 
 describe("board", function()
 
-  it('initializes with blank spaces', function()
+  before_each(function()
     board = Board:new()
+  end)
+
+  it('initializes with blank spaces', function()
     for i = 1, 9 do
       assert.are.equal(board:get(i), " ")
     end
+  end)
+
+  it('writes and reads a mark on a space', function()
+    board:set(2, "X")
+    assert.are.equal(board:get(2),"X")
+    board:set( 6, "O")
+    assert.are.equal(board:get(6),"O")
+  end)
+
+  it('can undo a move', function()
+    board:set(1,"X")
+    assert.are.same(board:current_state(), {"X"," "," "," "," "," "," "," "," "})
+    board:undo_move(1)
+    assert.are.same(board:current_state(), {" "," "," "," "," "," "," "," "," "})
   end)
 end)
--- src/board.lua
+++ src/board.lua
@@ -6,6 +6,18 @@ function Board:initialize()
   cells = {" ", " ", " ", " ", " ", " ", " ", " ", " "}
 end
 
+function Board:set(cell, mark)
+  cells[cell] = mark
+end
+
 function Board:get(cell)
   return cells[cell]
+end
+
+function Board:current_state()
+  return cells
+end
+
+function Board:undo_move(cell)
+  cells[cell] = " "
 end
