a06be39190fea3cc669d077c77f39be91ed3accb
Angeleah Daidone <angeleah@gmail.com>
2013-07-18 22:36:36 -0500

    Add ability to determine winning player or if game was a draw.

--- src/scoring.lua
+++ src/scoring.lua
@@ -11,6 +11,15 @@ function Scoring:winner(board)
   return winner
 end
 
+function Scoring:winning_mark(board)
+  for i,v in ipairs(board:possible_winning_combinations()) do
+    if v[1] == v[2] and v[2] == v[3] and v[1] ~= " " then
+      win_mark = v[1]
+      return win_mark
+    end
+  end
+end
+
 function group_equal(group)
   if group[1] == group[2] and group[2] == group[3] and group[1] ~= " " then
     winner = true
@@ -18,4 +27,8 @@ function group_equal(group)
   else
     return false
   end
+end
+
+function Scoring:draw(board)
+  return Scoring:winner(board) == false and #board:available_spaces() == 0
 end
