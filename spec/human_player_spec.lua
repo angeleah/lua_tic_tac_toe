afbcf9aceaf6a16ce59ed9f1e7940f60ecc1d6fe
Angeleah Daidone <angeleah@gmail.com>
2013-07-19 09:08:29 -0500

    Add use of Board to HumanPlayer spec

--- spec/human_player_spec.lua
+++ spec/human_player_spec.lua
@@ -6,6 +6,7 @@ describe('HumanPlayer', function()
 
   it('returns false when get move is called', function()
     human_player = HumanPlayer:new("X")
-    assert.False(human_player:get_move())
+    board = Board:new()
+    assert.False(human_player:get_move(board))
   end)
 end)
