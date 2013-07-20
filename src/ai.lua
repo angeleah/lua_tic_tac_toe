5c8c7ffd6ff690516b0e744b15989135507c39d5
Angeleah Daidone <angeleah@gmail.com>
2013-07-20 01:43:34 -0500

    debug minimax

--- src/ai.lua
+++ src/ai.lua
@@ -2,6 +2,8 @@ require 'board'
 require 'middleclass'
 require 'scoring'
 
+local inspect = require 'inspect'
+
 Ai = class('Ai')
 
 function Ai:initialize(a_player, a_board)
@@ -9,17 +11,15 @@ function Ai:initialize(a_player, a_board)
   scoring = Scoring:new(board)
   player = a_player
   max_mark = player.mark()
-end
-
-function Ai:find_best_move(cell, mark)
-  return 8
+  min_mark = ''
 end
 
 function Ai:find_opponent_mark()
   local marks = board:current_state()
   for _,v in pairs(marks) do
     if v ~= " " and v ~= max_mark then
-      return v
+      min_mark = v
+      return min_mark
     end
   end
 end
@@ -29,6 +29,63 @@ function Ai:select_optimal_start_move()
   i = math.random(#moves)
   return moves[i]
 end
+
+function Ai:find_best_move()
+  if (#board:available_spaces()) == 9 then
+    best_move = Ai:select_optimal_start_move()
+  else
+    Ai:find_opponent_mark()
+    move_reference, best_score = Ai:max_move()
+    best_move = move_reference
+  end
+  return best_move
+end
+
+function Ai:max_move()
+  best_move = nil
+  best_score = nil
+  for _,move in pairs(board:available_spaces()) do
+    board:set(move, max_mark)
+     -- print(inspect(board:available_spaces()))
+     -- print(inspect(Ai:state_is_terminal()))
+    if Ai:state_is_terminal() then
+      score = Ai:evaluate_the_board()
+    else
+      move_reference, score = Ai:min_move()
+    end
+    board:undo_move(move)
+    if best_score == nil or score > best_score then
+      best_score = score
+      best_move = move
+    end
+  end
+  return best_move, best_score
+end
+
+function Ai:min_move()
+  best_move = nil
+  best_score = nil
+    -- print(inspect(board:available_spaces()))
+     -- print(inspect(Ai:state_is_terminal()))
+  for _,move in pairs(board:available_spaces()) do
+    -- print(max_mark)
+    board:set(move, min_mark)
+    -- print(inspect(board:available_spaces()))
+    print(Ai:state_is_terminal())
+      if Ai:state_is_terminal() then
+        score = Ai:evaluate_the_board()
+      else
+        move_reference, score = Ai:max_move()
+      end
+    board:undo_move(move)
+    if best_score == nil or score < best_score then
+      best_score = score
+      best_move = move
+    end
+  end
+  return best_move, best_score
+end
+
 
 function Ai:state_is_terminal()
   return scoring:winner() or scoring.draw()
@@ -40,6 +97,6 @@ function Ai:evaluate_the_board()
   elseif scoring:winner() and scoring.winning_mark() == max_mark then
     return 1
   elseif scoring.draw() then
-      return 0
+    return 0
   end
 end
